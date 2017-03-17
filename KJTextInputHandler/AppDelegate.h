d6accd94a05bb5b02c4106756acb7bb0df03fce7
mm <mm@mmdemac-mini.local>
2017-03-17 16:02:58 -0700

    Initial Commit

---
+++ KJTextInputHandler/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KJTextInputHandler
+//
+//  Created by mm on 17/3/17.
+//  Copyright © 2017年 mm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KJTextInputHandler/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KJTextInputHandler
+//
+//  Created by mm on 17/3/17.
+//  Copyright © 2017年 mm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
